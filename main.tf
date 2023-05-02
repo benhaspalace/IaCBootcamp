08efae38799aab82435968d43206495f263c8f7c
Id3mdEp7FzB1Z83a <bencepalotas+github@gmail.com>
2023-05-02 13:40:36 +0200

    ADD Day 1 lab configuration files

---
+++ main.tf
@@ -0,0 +1,76 @@
+# Resource group
+resource "azurerm_resource_group" "IaCBootcampRG" {
+  name     = "IaCBootcampRG"
+  location = "North Europe"
+}
+
+#Network Security Group
+resource "azurerm_network_security_group" "IaCBootcampNSG" {
+  name                = "IaCBootcampNSG"
+  location            = azurerm_resource_group.IaCBootcampRG.location
+  resource_group_name = azurerm_resource_group.IaCBootcampRG.name
+}
+
+# Virtual network
+resource "azurerm_virtual_network" "IaCBootcampVNet" {
+  name                = "IaCBootcampVNet"
+  location            = azurerm_resource_group.IaCBootcampRG.location
+  resource_group_name = azurerm_resource_group.IaCBootcampRG.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
+
+# Subnet
+resource "azurerm_subnet" "IaCBootcampSubnet01" {
+  name                 = "IaCBootcampSubnet01"
+  resource_group_name  = azurerm_resource_group.IaCBootcampRG.name
+  virtual_network_name = azurerm_virtual_network.IaCBootcampVNet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+# Network interface card
+resource "azurerm_network_interface" "IaCBootcampVM01NIC" {
+  name                = "IaCBootcampVM01NIC"
+  location            = azurerm_resource_group.IaCBootcampRG.location
+  resource_group_name = azurerm_resource_group.IaCBootcampRG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.IaCBootcampSubnet01.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Virtual machine
+resource "azurerm_windows_virtual_machine" "IaCBootcampRGVM01" {
+  name                = "IaCBootcampVM01"
+  resource_group_name = azurerm_resource_group.IaCBootcampRG.name
+  location            = azurerm_resource_group.IaCBootcampRG.location
+  size                = "Standard_B2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.IaCBootcampVM01NIC.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
