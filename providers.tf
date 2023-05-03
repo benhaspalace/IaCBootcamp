f7662aab5f1aef48882e30950f6fef1a0ccf7968
benhaspalace <bencepalotas+github@gmail.com>
2023-05-03 10:32:41 +0200

    ADD backend configuration skeleton
    Save tfstate to an azure storage account

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "value"
+    storage_account_name = "value"
+    container_name = "value"
+    subscription_id = "value"
+    key = "value"
+  }
 }
 
 provider "azurerm" {
